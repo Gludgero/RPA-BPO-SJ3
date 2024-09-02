b6db0aa2d286d883e8c87156bdec71f7329424b6
GLudgero <guiludgero@sj3.com.br>
2024-09-02 16:22:55 -0300

    Avançando

--- ProjetoLancRPA.py
+++ ProjetoLancRPA.py
@@ -1,16 +1,29 @@
 import pyautogui as py ### Fazendo o código a partir da ideia do arquivo exe
 import time as tm
 
-### (Fazer depois) Abrir o sistema, planilha
+### (Fazer depois) Abrir o sistema, planilha   
 
-empresaAtual = "5" ### Lúdico
-clienteAtual = py.prompt(text="Qual o cliente atual?")
-serieAtual = "DRE"
-dataAtual = "28082024"
+empresaAtual = "3" ### 
+
+#clienteAtual = py.prompt(text="Qual o cliente atual?") ##################################
+clienteAtual = "MEGA"
+
+#serieAtual = "DRE" 
+serieAtual = 'DPA'
+
+dataAtual = "30082024"
+
 portadorAtual = "4"
-contaAtual = py.prompt(text="Qual a conta atual?")
-valorAtual = py.prompt(text="Qual o valor atual?")
-obs = py.prompt(text="Qual a obs atual?")
+
+#contaAtual = py.prompt(text="Qual a conta atual?") ##############################
+contaAtual = "99"
+
+#valorAtual = py.prompt(text="Qual o valor atual?") ########################
+valorAtual = "2591,50"
+
+#obs = py.prompt(text="Qual a obs atual?") ########################################
+obs = "NOVA MEGA G ATACADISTA DE A"
+
 nLinhas = 0
 prompt = ""
 
@@ -33,17 +46,23 @@ def abrirCARdoMenu():
     sleep2()
     py.click(922, 434)
 
-def SelectLancTitulos():
+def SelectLancTitulosCAR():
     ### Selecionar lançamentos, 
-    sleep2()
-    sleep2()
-    sleep2()
     sleep2()
     sleep2()
     py.click(65, 808)
     ### títulos a receber
     sleep2()
     py.click(71, 202)
+
+def selectLancTitulosCAP():
+    ### Selecionar lançamentos, 
+    sleep2()
+    sleep2()
+    py.click(59, 778)
+    ### títulos a pagar
+    sleep2()
+    py.click(104, 250)
 
 def abrirPlanilha():
     ### A planilha precisa estar no primeiro "slot" da barra de ferramentas
@@ -126,7 +145,7 @@ def selectParcelas():
     py.press('enter')
     sleep1()
 
-def selectOBS():
+def selectObsRecebimentos():
     i = 0
     while(i != 8):
         py.press('tab')
@@ -136,14 +155,24 @@ def selectOBS():
     py.write(obs)
     sleep1()
 
+def selectObsPagamentos():
+    i = 0
+    while(i != 5):
+        py.press('tab')
+        sleep1()
+        i = i + 1
+
+    py.write(obs)
+    sleep1()
+
 def salvaLanc():
     sleep1()
-    py.cick()
+    py.click()
     sleep2()
 
 def lancRecebimentos():
 
-    SelectLancTitulos() ### Selecionar "lançamentos" e "títulos"
+    SelectLancTitulosCAR() ### Selecionar "lançamentos" e "títulos"
 
     selectEmpresa() ### Selecionar a empresa
 
@@ -164,7 +193,39 @@ def lancRecebimentos():
 
     PrintDataTab() ### colocar a mesma data que as outras (por enquanto)
 
-    selectOBS() ### Colocar a obs com o historico da planilha, sem lanc sj3
+    selectObsRecebimentos() ### Colocar a obs com o historico da planilha, sem lanc sj3
+
+    prompt = py.prompt(text="Digite abaixo -salvar- para confirmar o lançamento?")
+
+    if(prompt == "salvar"):
+        salvaLanc() ### Salvar
+
+def lancPagamentos(): #######
+
+    selectLancTitulosCAP() ### Selecionar "lançamentos" e "títulos"
+
+    selectEmpresa() ### Selecionar a empresa
+
+    selectCliente() ### Selecionar o cliente
+
+    selectSerie()  ### Seleciona a serie
+
+    PrintDataTab() ### Selecionar a data 3 vezes
+    PrintDataTab()
+    PrintDataTab()
+    py.press('tab')
+    
+    selectPortador() ### Selecionar o portador (banco)
+
+    selectConta() ### Selecionar a conta gerencial 
+
+    selectValor() ### Selecionar o valor 
+
+    selectParcelas() ### Selecionar parcela 1
+
+    PrintDataTab() ### colocar a mesma data que as outras (por enquanto)
+
+    selectObsPagamentos() ### Colocar a obs com o historico da planilha, sem lanc sj3
 
     prompt = py.prompt(text="Digite abaixo -salvar- para confirmar o lançamento?")
 
@@ -172,19 +233,17 @@ def lancRecebimentos():
         salvaLanc() ### Salvar
 
 def minimizaVScode():
-    sleep1
+    sleep2
     py.click(1804, 17)
-    sleep1
-
+    sleep2
 
 def receberValores():
     sleep1()
     nLinhas = py.prompt(text="Qual o número de lançamentos você quer fazer?")
 
-
-
-
 def main():
+
+    sleep1
     py.alert(text="Cuidado com seu mouse e Teclado, o programa vai rodar!")
     sleep1
 
@@ -193,8 +252,10 @@ def main():
     #abrirCARdoMenu()
 
     #py.alert(text="Você confirmou se alguns dos lançamentos já foram feitos?" )  
-    
-    lancRecebimentos()
+
+    #lancRecebimentos()
+
+    lancPagamentos() ### Adicionar algo pra limpar a caixa de condições de pagamento 
 
     fecharSistema() 
 
